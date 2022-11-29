d66331f085bf4156b508d2be3e7681ed6e010a74
Katharina Fey <kookie@spacekookie.de>
2022-11-29 12:48:30 +0200

    Add explanatory comments to ncdu example solution

--- courses/day-02-builders/excercises/solutions/ncdu.nix
+++ courses/day-02-builders/excercises/solutions/ncdu.nix
@@ -8,18 +8,24 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-Xkr49rzYz3rY/T15ANqxMgdFoEUxAenjdPmnf3Ku0UE=";
   };
 
-  XDG_CACHE_HOME = "Cache";
-  # HOME = "$TMPDIR";
+  # In order to debug why the Zig build fails (with "Permission
+  # Denied"), include strace in the build environment
+  # (nativeBuildInputs) and use it to show which files and/or
+  # directories the compiler is trying to open.
 
-  PREFIX = placeholder "out";
-  
-  buildPhase = ''
-    mkdir -p $out
-    zig build
-  '';
+  # buildPhase = ''
+  #   ${pkgs.strace}/bin/strace zig build
+  # '';
 
-  
-  nativeBuildInputs = [ pkgs.zig ];
-
+  nativeBuildInputs = with pkgs; [ zig /* strace */ ];
   buildInputs = [ pkgs.ncurses ];
+
+  # The following environment variables must be set in the build
+  # environment to make the build succeed.  XDG_CACHE_HOME (or HOME)
+  # changes where Zig tries to store temporary files.  PREFIX must be
+  # changed to the output directory (Zig by default wants to install
+  # into /usr)
+
+  XDG_CACHE_HOME = "Cache";
+  PREFIX = placeholder "out";
 }
